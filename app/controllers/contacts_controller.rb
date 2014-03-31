1066107270f5b5f261409696076813a258aecfea
Brentwood Reid <bwoodreid@gmail.com>
2014-03-30 22:24:56 -0700

    initial commit.  Follow along with the tutorial

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,53 @@
+class ContactsController < ApplicationController
+
+	def index
+		@contacts = Contact.all
+		render('contacts/index.html.erb')
+	end
+
+	def show
+		@contact = Contact.new
+		render('contacts/show.html.erb')
+	end
+
+	def new
+    @contact = Contact.new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                           :email => params[:email],
+                           :phone => params[:phone])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+	end
+
+	def edit
+		@contact = Contact.find(params[:id])
+		render('contacts/edit.html.erb')
+	end
+
+	def update
+		@contact = Contact.find(params[:id])
+		if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+			render('contacts/success.html.erb')
+		else
+			render('contacts/edit.html.erb')
+		end
+	end
+
+	def destroy
+		@contact = contact.find(params[:id])
+		@contact.destroy
+		render('contacts/destroy.html.erb')
+	end
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = 'a67392853dcfe2f57fd62a56669e8e5f13975e4cb66cb259c9f5c29f949d9b88d98f67977e108674d32b4d7f9967f1f10a9ef6207743504f9ee90868cab496e8'
