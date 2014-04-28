97c1054eeec2905430d419db34faaa2f8f5221cd
Harland Hauger & Brentwood Reid <student@epicodus.com>
2014-04-28 09:05:27 -0700

    Refactor contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,62 +2,36 @@ class ContactsController < ApplicationController
 
 	def index
 		@contacts = Contact.all
-		
-		respond_to do |format|
-      format.html
-      format.json { render json: @contacts }
-    end
+    render json: @contacts
 	end
-
-	def show
-		@contact = Contact.find(params[:id])
-
-		respond_to do |format|
-			format.html
-			format.json { render json: @contact}
-		end
-	end
-
-	def new
-    @contact = Contact.new
-  end
 
   def create
     @contact = Contact.new(params[:contact])
     if @contact.save
-      flash[:notice] = "Contact created."
-      redirect_to contacts_path
+      render json: @contact, status: 201
     else
-      render 'new'
+      render json: @contact.errors, status: 422
+	end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render json: @contact
     end
-	end
-
-	def edit
-		@contact = Contact.find(params[:id])
-
-	end
+  end
 
 	def update
 		@contact = Contact.find(params[:id])
 		if @contact.update(params[:contact])
-			flash[:notice] = "Contact updated."
-			redirect_to contact_path(@contact)
+			head :no_content
 		else
-			render 'edit'
+			render jason: @contact.errors, status: 422
 		end
 	end
 
 	def destroy
 		@contact = contact.find(params[:id])
 		@contact.destroy
-
-		respond_to do |format|
-			format.html do
-				flash[:notice] = "Contact deleted"
-				redirect_to contacts_path
-			end
-			format.json { head :no_content }
-		end
+    head :no_content
 	end
 end
 
